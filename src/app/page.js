5e83bf489e69032cd12a5bb766bdd7c13281f626
lausofdiaz <144862180+lausofdiaz@users.noreply.github.com>
2023-10-01 00:25:19 -0500

    p

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
-        <div>
+        <div>ajjaja
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
